9ea176d558d0dafec25c6a6e0ca512b3ed211206
jaime0717-cpu <jaimelim0717@gmail.com>
2026-05-27 13:46:18 +0900

    commit

--- code/2-6.c
+++ code/2-6.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //여기에 신기한 게 많다. strstr 신기하다.
 
 int main(int argc, const char *argv[])
 {
@@ -9,19 +9,13 @@ int main(int argc, const char *argv[])
     FILE *fp = fopen(argv[1], "r");
     const char *target_str = argv[2];
     // here
-    char line[100];
+    char line[300];
 
     while (feof(fp) == 0)
     {
         fgets(line, sizeof line, fp);
-        for (int i = 0; i < sizeof line; i++)
-        {
-            for (int j = 0; j < strlen(target_str); j++)
-            {
-                char* cmp = 
-            }
-            //if (strcmp(line[i+j], ))
-        }
+        if(strstr(line,target_str)!=NULL)
+        printf("%s", line);
     }
 
     fclose(fp);
